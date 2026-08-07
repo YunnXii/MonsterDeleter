3985dc3d699d7097765b4f3bf3a8ecf77dc885d3
YunnXii <101539801+YunnXii@users.noreply.github.com>
2026-08-07 17:53:37 +0800

    build: embed generated Jiaqi head icon

--- build.ps1
+++ build.ps1
@@ -1,12 +1,16 @@
 $ErrorActionPreference = "Stop"
 
+python tools/build_app_icon.py
+
 python -m PyInstaller `
   --noconfirm `
   --clean `
   --onefile `
   --windowed `
   --name "叫家琦来" `
+  --icon "build\app.ico" `
   --add-data "characters;characters" `
+  --add-data "build\app.ico;assets" `
   --hidden-import send2trash `
   main.py
 
